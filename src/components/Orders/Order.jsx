f475cb9734ee5529f7ebe717ce31589b0cbcb57c
robiul1hossen <robim6225@gmail.com>
2023-04-05 00:53:26 +0600

    review item style added

--- src/components/Orders/Order.jsx
+++ src/components/Orders/Order.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Order.css";
 
 const Order = () => {
   const cart = useLoaderData();
   return (
     <div className="shop-container">
-      <div className="product-container">
-        <h2>This is order page : {cart.length}</h2>
+      <div className="order-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import "./ReviewItem.css";
 
-const ReviewItem = () => {
-  return <div>review item</div>;
+const ReviewItem = ({ product }) => {
+  const { id, img, name, price, quantity } = product;
+  return (
+    <div>
+      <div className="review-item">
+        <div>
+          <img src={img} alt="" />
+        </div>
+        <div className="review-info">
+          <p>{name}</p>
+          <p>
+            Price : <span className="text-orange">$ {price}</span>{" "}
+          </p>
+          <p>
+            Quantity : <span className="text-orange"> {quantity}</span>
+          </p>
+        </div>
+        <div>
+          <button className="btn-delete">
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewItem;
